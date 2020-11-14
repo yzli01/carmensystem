8cba510076d725459d04a91cd8c36aee018a51ad
yzli <li.yu.zhan@qq.com>
2020-11-14 18:16:16 +0800

    修订首页时间线

--- carmensysweb/src/components/home/AppIndex.vue
+++ carmensysweb/src/components/home/AppIndex.vue
@@ -1,13 +1,30 @@
 <template>
-  <div></div>
+  <div class="block">
+    <el-timeline>
+      <el-timeline-item v-for="(item,i) in timeList" :key="i" :timestamp="item.time" placement="top">
+        <el-card>
+          <h4>{{item.timeItem}}</h4>
+          <p>{{item.timeDone}}</p>
+        </el-card>
+      </el-timeline-item>
+    </el-timeline>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'AppIndex'
+  name: 'AppIndex',
+  data () {
+    return {
+      timeList: [
+        {time: '2020/8/1', timeItem: '买包包', timeDone: 'Carmen 提交于 2020/1/1 20:46'},
+        {time: '2020/4/3', timeItem: '愿望列表', timeDone: 'Carmen 提交于 2020/4/3 20:46'},
+        {time: '2020/4/2', timeItem: '亲亲', timeDone: 'Carmen 提交于 2018/4/2 20:46'}
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
--- carmensysweb/src/components/person/Person.vue
+++ carmensysweb/src/components/person/Person.vue
@@ -1,10 +1,21 @@
 <template>
-  <div></div>
+  <el-container>
+    <el-header></el-header>
+    <el-main>
+      <el-calendar v-model="value">
+      </el-calendar>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 export default {
-  name: 'Person'
+  name: 'Person',
+  data () {
+    return {
+      value: new Date()
+    }
+  }
 }
 </script>
 
